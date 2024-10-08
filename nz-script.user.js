0fa28844b0420db330d71d33b363eab92e24903a
Noob <111202059+Noob-Lol@users.noreply.github.com>
2024-10-08 20:04:31 +0300

    wrong url fix

--- nz-script.user.js
+++ nz-script.user.js
@@ -8,7 +8,7 @@
 // @grant        none
 // @run-at       document-start
 // @icon         https://static.wikia.nocookie.net/f0e6ab88-6fb8-42c6-8ca6-7b5cfb180363/scale-to-width/370
-// @downloadURL  https://github.com/Noob-Lol/a/raw/main/nz-script.user.js
+// @downloadURL  https://github.com/Noob-Lol/a/raw/refs/heads/main/nz-script.user.js
 // ==/UserScript==
 
 (function() {
