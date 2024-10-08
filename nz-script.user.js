78dd1f0298a4bc1563a658c95e3e4a1dd9c8ce4a
Noob <111202059+Noob-Lol@users.noreply.github.com>
2024-10-08 20:12:20 +0300

    Update nz-script.user.js

--- nz-script.user.js
+++ nz-script.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         insane script
-// @namespace    https://github.com/Noob-Lol
+// @namespace    Violentmonkey Scripts
 // @version      228
 // @description  It works?
 // @author       Noob
@@ -8,7 +8,9 @@
 // @grant        none
 // @run-at       document-start
 // @icon         https://static.wikia.nocookie.net/f0e6ab88-6fb8-42c6-8ca6-7b5cfb180363/scale-to-width/370
+// @homepageURL  https://github.com/Noob-Lol
 // @downloadURL  https://github.com/Noob-Lol/a/raw/refs/heads/main/nz-script.user.js
+// @supportURL   https://github.com/Noob-Lol/a/blob/main/README.md
 // ==/UserScript==
 
 (function() {
